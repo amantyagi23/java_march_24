f18630a5a644eecd0cd7383d242b517313884dbc
Aman <119286791+amantyagi23@users.noreply.github.com>
2024-03-19 12:59:41 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,31 @@
+class Example{
+public static void main(String[] args) {
+
+    // variable
+    // datatype
+    // function/method
+    // return type
+    
+
+    int a = 10;
+
+    // a -> variable
+    // variable store the address value 
+    // value always store on ram and return address
+
+    int name12 = 10;
+
+    datatype
+    
+
+
+
+    System.out.println(a-10);
+}
+}
+
+
+class Example2 {
+
+    
+}
